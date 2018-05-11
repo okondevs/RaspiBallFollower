a28670f1908d2d01b8ed6043c7dd748723cfb315
KPl <stg.krzysztof.plucinski@gmail.com>
2018-05-11 12:33:09 +0200

    Added new methods for StepMotor

--- StepMotor.py
+++ StepMotor.py
@@ -1,10 +1,11 @@
 import pigpio
 import time
 
-FORWARD = 2
-BACKWARD = -1
+FORWARD = 2 	#can be 1 or 2
+BACKWARD = -1	#can be -1 or -2
 
 class StepMotor:
+
 	def __init__(self, out1, out2, out3, out4, **kwargs):
 		if "pi" in kwargs and "port" in kwargs:
 			self.pi = pigpio.pi(kwargs['pi'], kwargs['port'])
@@ -26,8 +27,34 @@ class StepMotor:
 		self.step_count = len(self.sequence)
 		self.direction = FORWARD
 
-	def forward(self, steps):
-		for i in range(0, steps * 2):
+
+	def move_forward_for_angle(self, angle):
+		self.move_forward(self._calculate_steps(angle))
+
+
+	def move_backward_for_angle(self, angle):
+		self.backward(self._calculate_steps(angle))
+
+
+	def _calculate_steps(self, angle):
+		steps = int(4076 * (angle / 360))
+		print(steps)
+		return steps
+
+
+	def move_forward(self, steps):
+		'''For full revolution step motor does 4076 steps'''
+		self.direction = FORWARD
+		self._move(steps)
+
+
+	def move_backward(self, steps):
+		self.direction = BACKWARD
+		self._move(steps)
+
+
+	def _move(self, steps):
+		for i in range(0, steps):
 			for pin in range(0, 4):
 				if self.sequence[i % 8][pin] != 0:
 					self.pi.write(self.step_pins[pin], True)
@@ -36,7 +63,7 @@ class StepMotor:
 			time.sleep(0.001)
 
 
+
 if __name__ == "__main__":
-	print("Hello main")
 	sm = StepMotor(12, 16, 20, 21)
-	sm.forward(4076)
+	sm.move_forward(4076)
