5a45d0b8488f39b9f570a58a1ce34d62a8c4552d
karbol <lukaszkarbowiak93@gmail.com>
2018-05-13 15:20:42 +0000

    TrackerBoosting implemented

--- Tracking.py
+++ Tracking.py
@@ -11,20 +11,36 @@ params.filterByConvexity  = False
 params.filterByColor = False
 
 detector = cv2.SimpleBlobDetector_create(params)
+
+tracker = cv2.TrackerBoosting_create()
+
 cap = cv2.VideoCapture(0)
+
 i = 10
 while(i):
     ret, frame = cap.read() #camera init
     i = i - 1
+#while(True):
+ret, frame = cap.read()
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+a, b, c = cv2.split(hsv)
+ret,thresh1 = cv2.threshold(b,120,255,cv2.THRESH_BINARY)
+kernel = np.ones((5,5),np.uint8)
+thresh1 = cv2.erode(thresh1,kernel,iterations = 2)
+keypoints = detector.detect(thresh1)
+print(keypoints[0].pt[0])
+boxObject = ( keypoints[0].pt[0]-30, keypoints[0].pt[1]-30 , 60 , 60 )
+p1 = (int(boxObject[0]), int(boxObject[1]))
+p2 = (int(boxObject[0] + boxObject[2]), int(boxObject[1] + boxObject[3]))
+tracker.init(frame, boxObject)
+cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+
 while(True):
 	ret, frame = cap.read()
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-	a, b, c = cv2.split(hsv)
-	ret,thresh1 = cv2.threshold(b,120,255,cv2.THRESH_BINARY)
-	kernel = np.ones((5,5),np.uint8)
-	thresh1 = cv2.erode(thresh1,kernel,iterations = 2)
-	keypoints = detector.detect(thresh1)
-	#print(keypoints[0].pt[0])
-	im_with_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS )
-	cv2.imshow('frame', im_with_keypoints)
+	ok, boxObject = tracker.update(frame)
+	p1 = (int(boxObject[0]), int(boxObject[1]))
+	p2 = (int(boxObject[0] + boxObject[2]), int(boxObject[1] + boxObject[3]))
+	cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+	#print(boxObject)
+	cv2.imshow('frame', frame)
 	cv2.waitKey(10)
