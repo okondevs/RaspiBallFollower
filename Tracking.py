28a5f478d948747f03b7c8529e72083a7e142e59
karbol <lukaszkarbowiak93@gmail.com>
2018-05-16 20:03:33 +0000

    Continued of rewrite code to oop

--- Tracking.py
+++ Tracking.py
@@ -11,34 +11,45 @@ import numpy as np
 
 class Tracking:
 	""" Find and track ball """
-	
-	tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
-	
-	def __init__ (self, tracker_types, minSize, maxSize):
-		""" Create type of tracking and set size of this object
+
+	def __init__ (self):
+		""" Create type of tracker
+		
+			The best type of tracker for this project is KCF Tracker
+			
+			Other type of tracker:
+			BOOSTING
+			MIL
+			TLD
+			MEDIANFLOW
+			GOTORUN -> only this used CNN (Unfortunately can't used with OpenCV 3.2 because crashes the program)
+		
+		"""	
+		self.tracker = cv2.TrackerKCF_create()
+			
+	def init_track_object (self, frame, objectArea):
+		""" Set object which will be tracking 
 		
 			Args:
-				tracker_types: type of tracking methods
-				minSize: minimal size of object which will be tracking
-				maxSize: maximal size of object which will be tracking
-		"""	
-		if tracker_type == 'BOOSTING':
-			tracker = cv2.TrackerBoosting_create()
-		if tracker_type == 'MIL':
-			tracker = cv2.TrackerMIL_create()
-		if tracker_type == 'KCF':
-			tracker = cv2.TrackerKCF_create()
-		if tracker_type == 'TLD':
-			tracker = cv2.TrackerTLD_create()
-		if tracker_type == 'MEDIANFLOW':
-			tracker = cv2.TrackerMedianFlow_create()
-		if tracker_type == 'GOTURN':
-			tracker = cv2.TrackerGOTURN_create()
+				frame : frame from camera
+				objectArea (rectangle): area of frame which contain object to track
+			
+		"""
+		self.tracker.init(frame, boxObject)
+		
+	def tracker_update (self, frame):
+		""" Update position of tracked object
+		
+			Args:
+				frame: frame from camera
+				
+		"""
+		return tracker.update(frame)
 	
 	
 params = cv2.SimpleBlobDetector_Params()
 params.filterByArea = True
-params.minArea = 800
+params.minArea = 100
 params.maxArea = 99999999
 params.filterByInertia = False
 params.filterByCircularity  = False
@@ -47,28 +58,33 @@ params.filterByColor = False
 
 detector = cv2.SimpleBlobDetector_create(params)
 
-tracker = cv2.TrackerBoosting_create()
+tracker = cv2.TrackerKCF_create()
 
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-i = 10
+print(cap.isOpened())
+i = 20
 while(i):
     ret, frame = cap.read() #camera init
     i = i - 1
 #while(True):
 ret, frame = cap.read()
-hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-a, b, c = cv2.split(hsv)
-ret,thresh1 = cv2.threshold(b,120,255,cv2.THRESH_BINARY)
+hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+a, b, c = cv2.split(frame)
+ret,thresh1 = cv2.threshold(c,90,255,cv2.THRESH_BINARY)
 kernel = np.ones((5,5),np.uint8)
 thresh1 = cv2.erode(thresh1,kernel,iterations = 2)
 keypoints = detector.detect(thresh1)
-print(keypoints[0].pt[0])
+#	print(keypoints[0].pt[0])
 boxObject = ( keypoints[0].pt[0]-30, keypoints[0].pt[1]-30 , 60 , 60 )
 p1 = (int(boxObject[0]), int(boxObject[1]))
 p2 = (int(boxObject[0] + boxObject[2]), int(boxObject[1] + boxObject[3]))
 tracker.init(frame, boxObject)
 cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+cv2.imshow('frame', thresh1)
+cv2.waitKey(10)
 
 while(True):
 	ret, frame = cap.read()
